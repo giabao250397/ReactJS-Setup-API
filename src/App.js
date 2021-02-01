0d6e1599850ee1cd51e627acdf3a369ad837486a
gb <trangiabao250397@gmail.com>
2021-02-01 17:10:27 +0700

    Update title1233

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gia Bao
+          Gia Bao check item
         </p>
       </header>
     </div>
